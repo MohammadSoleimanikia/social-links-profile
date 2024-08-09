bd936541bfd461a94a0be298fa93e5482acb2377
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2024-08-09 06:02:18 +0330

    complete the codes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,20 @@
 module.exports = {
   content: ["./docs/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        secondaryGreen: "hsl(75, 94%, 57%)",
+        primarygray: {
+          700: "hsl(0, 0%, 20%)",
+          800: "hsl(0, 0%, 12%)",
+          900: "hsl(0, 0%, 8%)",
+        },
+      },
+      fontFamily: {
+        Inter: ["Inter", "sans-serif"],
+      
+      },
+    },
   },
   plugins: [],
-}
-
+};
